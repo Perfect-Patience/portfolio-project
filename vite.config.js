d20390d2ff21dbe765e87dfa49380eeac41bed9b
Perfect Dah <patiencedah@gmail.com>
2025-01-28 06:47:25 +0000

    try

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/portfolio-project/' 
-  : '/',
-    // base: '/portfolio-project/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio-project/' : '/',
   plugins: [
     vue(),
     vueDevTools(),

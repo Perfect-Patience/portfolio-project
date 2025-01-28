9ffdde3c18b766644607f1252c4ee77d941f87a5
Perfect Dah <patiencedah@gmail.com>
2025-01-28 02:37:00 +0000

    Adds Portfolio features     -Adds Home view     -Adds Project view     -Adss About page view     -Includes Social Medie links     -Inplements Responsiveness

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,62 @@
+
+<template>
+  <div class="text-gray-100 py-3.5 px-6 md:flex justify-between items-center">
+    <a href="/"><div class="flex items-center cursor-pointer">
+      <span class="text-gray-100 text-xl mr-1">
+        <i class="bi bi-code-square"></i>
+      </span>
+      <h1 class="text-xl px-2">Perfect</h1>
+    </div></a>
+    <span
+      class="absolute md:hidden right-6 top-1.5 cursor-pointer text-4xl"
+      @click="toggleMenu()"
+    >
+      <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']"></i>
+    </span>
+    <ul
+      class="md:flex md:items-center md:px-0 px-6 md:pd-0 pd-10 md:static absolute bg-slate-950 md:w-auto w-full top-14 duration-700 ease-in"
+      :class="[open ? 'left-0' : 'left-[-100%]']"
+    >
+      <li class="md:px-6 md:py-0 py-2" v-for="link in links" :key="link.name">
+        <a :href="link.link" class="text-xl hover:text-blue-600">{{
+          link.name
+        }}</a>
+      </li>
+      <li class="md:px-6 md:py-0 py-4">
+        <Button @click="sendEmail" class="cursor-pointer">Let's talk</Button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Button from "./BlueButton.vue";
+export default {
+  components: {
+    Button,
+  },
+  setup() {
+    let open = ref(false);
+    let links = [
+      { name: "Home", link: "/" },
+      { name: "About me", link: "/about" },
+      { name: "Projects", link: "/projects" },
+    ];
+
+    function toggleMenu() {
+      open.value = !open.value;
+    }
+    function sendEmail () {
+    const mailtoLink = `mailto:patiencedah@gmail.com`;
+    window.location.href = mailtoLink;
+    }
+    return {
+      links,
+      open,
+      toggleMenu, 
+      sendEmail,
+    };
+  },
+};
+</script>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="min-h-screen bg-slate-900 py-12">
+    <div class="container mx-auto px-6 grid gap-6">
+      <h1 class="text-4xl font-bold text-white mb-8 text-center mb-8">
+        Projects
+      </h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div
+          v-for="project in projects"
+          :key="project.id"
+          class="bg-white rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+        >
+          <img
+            :src="project.image"
+            :alt="project.title"
+            class="w-full h-48 object-cover"
+          />
+          <a
+            :href="project.link"
+            target="_blank"
+            class="text-blue-500 hover:no-underline"
+          >
+            <div class="p-4">
+              <h2 class="text-xl font-bold text-gray-800 mb-2">
+                {{ project.title }}
+              </h2>
+              <p class="text-gray-600 mb-4">{{ project.description }}</p>
+              View Project
+            </div></a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectsPage",
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          title: "DevJobs",
+          description:
+            "A web application showcasing job opportunities with a user-friendly interface. The application features Dark and Light modes that provide users with a customizable visual preference.",
+          image: "src/assets/devjobs.png",
+          link: "https://devjobs-o6nzcl5cp-perfect-patience.vercel.app",
+        },
+        {
+          id: 2,
+          title: "Portfolio Website",
+          description:
+            "A personal portfolio built with Vue.js and TailwindCSS.",
+          image: "src/assets/portfolio.png",
+          link: "https://perfect-patience.github.io/Portfolio-project/",
+        },
+        {
+          id: 3,
+          title: "Cloudinary Image Uploader",
+          description:
+            "A local Ghanaian restuarant application built with HTML, CSS and JavaScript ",
+          image: "src/assets/sankofa eats.png",
+          link: "https://perfect-patience.github.io/Sankofa_Eats/",
+        },
+        {
+          id: 4,
+          title: "SofaHouse",
+          description: "A figma design of a furniture E-commerce application",
+          image: "src/assets/sofaHouse.png",
+          link: "https://www.figma.com/design/ALbfVDlSpm8sqtk6j5gNQU/SofaHouse?node-id=0-1&t=EZXl6AZkCQeTWIXU-1",
+        },
+        {
+          id: 5,
+          title: "Qr-Code Component",
+          description: "A beginner friendly project from Frontend Mentor",
+          image: "src/assets/qr-code.png",
+          link: "https://perfect-patience.github.io/frontend-mentor-challenges/qr-code-component",
+        },
+        {
+          id: 6,
+          title: "Blog Card Preview",
+          description: "A beginner friendly project from Frontend Mentor",
+          image: "src/assets/card.png",
+          link: "https://perfect-patience.github.io/frontend-mentor-challenges/blog-preview-card",
+        },
+      ],
+    };
+  },
+};
+</script>
